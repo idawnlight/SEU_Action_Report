8535a99a47cae89a8142d3814694382eb974c5f3
idawnlight <idawn@live.com>
2021-09-12 19:54:19 +0800

    refactor
    insist on webdriver however

--- auto_temperature.py
+++ auto_temperature.py
@@ -4,140 +4,123 @@ from selenium import webdriver
 import random
 import time
 import datetime
+from pytz import timezone
 import os
 import sys
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path='chromedriver', options=chrome_options)
+
+if "ID" in os.environ:
+    username = os.environ["ID"]
+else:
+    print("未找到 ID")
+    sys.exit(1)
+
+if "PASSWORD" in os.environ:
+    password = os.environ["PASSWORD"]
+else:
+    print("未找到 PASSWORD")
+    sys.exit(1)
+
+t1 = 30
+t2 = 5
 
 
 def main():
-    global t
-    global msg
-    global error
-    global username
-    global password
-    global name
-    try:
-        date_time = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
-        print("时间:", date_time)
-        msg += "时间:\t" + str(date_time) + '\n\n'
+    tz = timezone('Asia/Shanghai')
+    date_time = datetime.datetime.now(tz).strftime("%Y-%m-%d, %H:%M:%S")
+    print("开始体温上报 @" + date_time)
 
-        driver = webdriver.Chrome(executable_path='chromedriver', options=chrome_options)
+    print("尝试登录...")
+    login()
 
-        url = "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/index.do?t_s=1583641506865#/dailyReport"
-        driver.get(url)
-        print(name + '\t' + "开始体温上报")
-        time.sleep(t)
+    print("打开表单")
+    open_form()
 
-        checkUrl = driver.current_url
-        if not checkUrl.startswith("http://ehall.seu.edu.cn/"):
-            driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
-            driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
-            driver.find_element_by_xpath('//*[@id="casLoginForm"]/p[5]/button').click()
-        time.sleep(t)
-
-        checkUrl = driver.current_url
-        if not checkUrl.startswith("http://ehall.seu.edu.cn/"):
-            print(name + '\t' + username + '\t' + password + '\t登录失败')
-            msg += name + '\t' + username + '\t' + password + '\t登录失败\n\n'
-            error = True
-            driver.quit()
-            return
-
-        print("成功登录")
-        driver.find_element_by_xpath('//div[@data-action="add"]').click()
-        time.sleep(t)
-
-        if '今日已填报！' in driver.page_source:
-            print(name + '\t' + '今日已填报！')
-            msg += name + '\t' + '今日已填报！' + '\n\n'
-            driver.quit()
-            return
-
-        elif '每日健康申报截止' in driver.page_source:
-            print(name + '\t' + '每日健康申报截止')
-            msg += name + '\t' + '每日健康申报截止' + '\n\n'
-            driver.quit()
-            return
-
-        print("添加")
-        time.sleep(t)
-        if "English Name" in  driver.page_source:
-            driver.find_element_by_xpath(
-                '/html/body/div[11]/div/div[1]/section/div[2]/div/div[3]/div[2]/div[8]/div[1]/div/input').send_keys(
-                round(random.uniform(36.3, 36.8), 1))
-        else:
-            driver.find_element_by_xpath(
-                '/html/body/div[11]/div/div[1]/section/div[2]/div/div[4]/div[2]/div[1]/div[1]/div/input').send_keys(
-                round(random.uniform(36.3, 36.8), 1))
-        print("输入体温")
-        time.sleep(t)
-
-        div = driver.find_element_by_xpath('//*[@id="save"]')
-        # 滑动滚动条到某个指定的元素
-        js4 = "arguments[0].scrollIntoView();"
-        # 将下拉滑动条滑动到当前div区域
-        driver.execute_script(js4, div)
-        time.sleep(t)
-
-        driver.find_element_by_xpath('//*[@id="save"]').click()
-        print("点击保存")
-        time.sleep(t)
-        if "确定数据无误并提交数据" in driver.page_source:
-            driver.find_element_by_xpath('/html/body/div[62]/div[1]/div[1]/div[2]/div[2]/a[1]').click()
-            print(name + '\t体温上报成功')
-            msg += name + '\t体温上报成功' + '\n\n'
-        else:
-           error = True
-           print(name + '\t请主动上报一次，来完善新增信息')
-           msg += name + '\t请主动上报一次，来完善新增信息' + '\n\n'
-        driver.quit()
-        return
-
-    except Exception as e:
-        msg += name + '\t' + username + '\t体温上报失败' + '\n\n' + str(e) + '\n\n'
-        print(name + '\t' + username + '\t体温上报失败' + '\n' + str(e))
-        error = True
-        driver.quit()
-        return
+    print("新增数据")
+    fill_form()
 
 
-if __name__ == '__main__':
-    if "ID" in os.environ:
-        username = os.environ["ID"]
+def login():
+    # Amazingly, ehall of seu doesn't support https
+    # using https will get a 404 error
+    url = "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/index.do#/dailyReport"
+    driver.get(url)
+    time.sleep(t1)
+
+    checkUrl = driver.current_url
+    if checkUrl.startswith("https://newids.seu.edu.cn/"):
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+        driver.find_element_by_xpath('//*[@id="casLoginForm"]//button[@type="submit"]').click()
     else:
-        sys.exit(1)
+        fail("未知的登录页")
+    time.sleep(t1)
 
-    if "PASSWORD" in os.environ:
-        password = os.environ["PASSWORD"]
+    checkUrl = driver.current_url
+    if not checkUrl.startswith("http://ehall.seu.edu.cn/"):
+        fail("登录失败")
+
+    print("登录成功")
+
+
+def open_form():
+    driver.find_element_by_xpath('//div[@data-action="add"]').click()
+    time.sleep(t1)
+
+    if '今日已填报' in driver.page_source:
+        success('今日已填报')
+    elif '每日健康申报截止' in driver.page_source:
+        fail('每日健康申报已截止')
+
+
+def fill_form():
+    temperature = round(random.uniform(36.3, 36.8), 1)
+
+    print("输入体温 " + str(temperature))
+
+    # TODO: better xpath
+    if "English Name" in driver.page_source:
+        driver.find_element_by_xpath(
+            '/html/body/div[11]/div/div[1]/section/div[2]/div/div[3]/div[2]/div[8]/div[1]/div/input').send_keys(
+            temperature)
     else:
-        sys.exit(1) 
+        driver.find_element_by_xpath(
+            '/html/body/div[11]/div/div[1]/section/div[2]/div/div[4]/div[2]/div[1]/div[1]/div/input').send_keys(
+            temperature)
+    time.sleep(t2)
 
-    if "NAME" in os.environ:
-        name = os.environ["NAME"]
+    div = driver.find_element_by_xpath('//*[@id="save"]')
+    # 将下拉滑动条滑动到当前 div 区域
+    driver.execute_script("arguments[0].scrollIntoView();", div)
+    time.sleep(t2)
+
+    driver.find_element_by_xpath('//*[@id="save"]').click()
+    print("点击保存")
+    time.sleep(t1)
+    if "确定数据无误并提交数据" in driver.page_source:
+        # TODO: better xpath
+        driver.find_element_by_xpath('/html/body/div[62]/div[1]/div[1]/div[2]/div[2]/a[1]').click()
+        success('体温上报成功')
     else:
-        name = "无名氏"
+        fail('请主动上报一次，来完善新增信息')
 
-    #time.sleep(random.randint(0, 300))
-    
-    error = False
-    t = 30
 
-    msg = '体温上报' + '\n\n'
+def success(msg):
+    print(msg)
+    driver.quit()
+    sys.exit(0)
+
+
+def fail(msg):
+    print(msg)
+    driver.quit()
+    sys.exit(1)
+
+
+if __name__ == "__main__":
     main()
-    while error:
-        error = False
-        t += 10
-        msg = '体温上报' + '\n\n'
-        main()
-        if t > 50:
-            break
-
-    if error:
-        subject = name + '\t' + '体温上报\t失败'
-        sys.exit(1)
-
-    else:
-        subject = name + '\t' + '体温上报\t成功'
